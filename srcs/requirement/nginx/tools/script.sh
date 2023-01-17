506d7bfdfd1044f0aff0ce3c2cb7fa0dc7514835
Abdelghani Abouelmahassine <ababouel@e1r4p8.1337.ma>
2023-01-17 22:54:35 +0100

    update wp and nginx

--- srcs/requirement/nginx/tools/script.sh
+++ srcs/requirement/nginx/tools/script.sh
@@ -4,3 +4,5 @@ openssl req -x509 -nodes -days 365\
 	-newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key\
 	-out /etc/ssl/certs/nginx-selfsigned.crt -subj \
 	"/C=MO/L=KHOURIBGA/O=1337/OU=student/CN=ababouel.42.fr";
+
+echo "ababouel.42.fr" > /etc/hostname
--- srcs/requirement/wordpress/tools/script.sh
+++ srcs/requirement/wordpress/tools/script.sh
@@ -1,19 +1,22 @@
 #! /bin/sh
-apt-get update && apt-get upgrade
-apt-get add bash
-apt-get add php81 php81-fpm php81-opcache
-apt-get add php81-gd php81-zlib php81-curl
-apt-get add tar wget
-/etc/init.d/php-fpm81 restart
-cd /tmp && wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-wp --info
-wp core download
-mkdir -p /var/www/html/ababouel.42.fr/public_html
-chown -R www-data:www-data /var/www/html/ababouel.42.fr/public_html
-chown www-data:www-data /var/www/html/example.com/public_html
-cd /var/www/html/ababouel.42.fr/public_html
--u www-data wp core download
--u www-data wp core config --dbname='wordpress' --dbuser='wpuser' --dbpass='password' --dbhost='localhost' --dbprefix='wp_'
--u www-data wp core install --url='http://ababouel.42.fr' --title='Blog 1337' --admin_user='adminuser' --admin_password='password' --admin_email='ababouel@student.1337.ma'
+apt-get update -y
+apt-get upgrade -y
+apt-get install  tar wget -y
+apt-get install php-fpm php-common php-mysql \
+php-xml php-xmlrpc php-curl php-gd \
+php-imagick php-cli php-dev php-imap \
+php-mbstring php-opcache php-redis \
+php-soap php-zip -y
+# /etc/init.d/php-fpm8.0 restart
+# cd /tmp && wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+# chmod +x wp-cli.phar
+# mv wp-cli.phar /usr/local/bin/wp
+# wp --info
+# wp core download
+# mkdir -p /var/www/html/ababouel.42.fr/public_html
+# chown -R www-data:www-data /var/www/html/ababouel.42.fr/public_html
+# chown www-data:www-data /var/www/html/example.com/public_html
+# cd /var/www/html/ababouel.42.fr/public_html
+# -u www-data wp core download
+# -u www-data wp core config --dbname='wordpress' --dbuser='wpuser' --dbpass='password' --dbhost='localhost' --dbprefix='wp_'
+# -u www-data wp core install --url='http://ababouel.42.fr' --title='Blog 1337' --admin_user='adminuser' --admin_password='password' --admin_email='ababouel@student.1337.ma'

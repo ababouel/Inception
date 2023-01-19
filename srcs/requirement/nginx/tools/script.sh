819d688e53bd9b9dbf134ac853242b4de918ebe0
Abdelghani <abdelghani@Abdelghanis-MacBook-Air.local>
2023-01-19 01:39:38 +0100

    update inception project

--- srcs/requirement/nginx/tools/script.sh
+++ srcs/requirement/nginx/tools/script.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
+apt-get update && apt-get -y install nginx && apt-get install -y openssl;
 
-openssl req -x509 -nodes -days 365\
-	-newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key\
-	-out /etc/ssl/certs/nginx-selfsigned.crt -subj \
-	"/C=MO/L=KHOURIBGA/O=1337/OU=student/CN=ababouel.42.fr";
-
-echo "ababouel.42.fr" > /etc/hostname
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout\
+ 		/etc/ssl/private/nginx-selfsigned.key -out\
+  		/etc/ssl/certs/nginx-selfsigned.crt \
+  		-subj="/CN=ababouel/O=ababouel.1337.ma/C=MA/L=KHOURIBGA";

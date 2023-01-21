78d4a36fc05a6d1b17635ce4b69ab84807d6282c
Abdelghani Abouelmahassine <ababouel@e1r4p1.1337.ma>
2023-01-21 05:04:27 +0100

    update mariadb

--- srcs/requirement/mariadb/tools/script.sh
+++ srcs/requirement/mariadb/tools/script.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
+sed -i 's/bind-address/#bind-adress/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# starting the mysql service
 service mysql start
 
-# change the bind to 0.0.0.0 only accept client connections made to 0.0.0.0 (accept connection to any address)
-sed -i 's/bind-address            = 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-sudo mkdir -p /var/run/mysqld/
-touch /var/run/mysqld/mysqld.pid
-touch /var/run/mysqld/mysqld.sock
-chown mysql:mysql -R /var/run/mysqld
-
-# create the database if not exist
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-
-# create the user if not exist
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-
-# grant all priviliges on the created database to the user
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-
-# this command tell the MySQL or MariaDB server to reload the grant tables and update its internal data structures with the current contents of the grant tables.
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "FLUSH PRIVILEGES;"
-
-# set the password to the root
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOTPASSWORD';"
-
-# killing the porcess of mysqld to not restarting while waiting the wordpress to get setup
+mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
+mysql -u root -p -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'" ;
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" ;
+mysql -u root -p -e "FLUSH PRIVILEGES;"
+mysql -u root -p -e "alter user 'root'@'localhost' identified by '$DB_ROOT_PASS';" ;
+mysql -u root -p -e "FLUSH PRIVILEGES;"
 kill `cat /var/run/mysqld/mysqld.pid`
+
+mysqld

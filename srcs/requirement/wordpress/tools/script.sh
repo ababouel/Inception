97a35ed3af3e038987d093331e6f7e192984c71b
Abdelghani Abouelmahassine <ababouel@e1r5p6.1337.ma>
2023-01-22 21:22:59 +0100

    update inception

--- srcs/requirement/wordpress/tools/script.sh
+++ srcs/requirement/wordpress/tools/script.sh
@@ -21,7 +21,7 @@ cd /var/www/html/
 wp core download --allow-root
 
 # create wp-config.php file
-# touch wp-config.php
+touch wp-config.php
 
 # copy the default configuration to wp-config.php
 cat /var/www/html/wp-config.php
@@ -31,17 +31,17 @@ cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 sed -i '36 s/\/run\/php\/php7.3-fpm.sock/9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
 # Set The Database that will be connected with wordpress
-sed -i 's/database_name_here/'$DB_NAME'/g' /var/www/html/wp-config.php
+sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
 
 # # Set the Username of The database
-sed -i 's/username_here/'$DB_USER'/g' /var/www/html/wp-config.php
+sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
 
 # Set the Password
-sed -i 's/password_here/'$DB_PASS'/g' /var/www/html/wp-config.php
+sed -i "s/password_here/$DB_PASS/g" /var/www/html/wp-config.php
 
 # # set The Hostname of the That base
-# sed -i 's/localhost/'$HOST'/g' /var/www/html/wp-config.php
-wp --allow-root core config --dbhost=mariadb --dbname=MYSQL_USER --dbuser=$DB_USER --dbpass=$DB_PASS
+sed -i "s/localhost/$DB_HOST/g" /var/www/html/wp-config.php
+wp --allow-root core config --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS
 chmod 777 /var/www/html/wp-config.php
 # wp config set FORCE_SSL_ADMIN 'false' --allow-root
 
